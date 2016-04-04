b6a9b9a302be76f708522f601feeed1d3670cac9
Dion Amago <dion@transition9.com>
2016-04-04 14:01:00 -0700

    Hands free source-map-support

--- js/npm/SourceMapSupport.hx
+++ js/npm/SourceMapSupport.hx
@@ -4,4 +4,12 @@ package js.npm;
 extern class SourceMapSupport
 {
 	public static function install() :Void;
+
+	private static function __init__() : Void untyped
+	{
+		var SourceMapSupport = Node.require('source-map-support');
+		SourceMapSupport.install();
+		//http://stackoverflow.com/questions/33778714/haxe-nodejs-debugging-source-mapping
+		haxe.CallStack.wrapCallSite = untyped __js__("require('source-map-support').wrapCallSite");
+	}
 }
