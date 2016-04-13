971ec293fd8c595470acdf36e868d95f26493e72
Dion Amago <dion@transition9.com>
2016-04-13 11:10:31 -0700

    Improved Busboy externs

--- js/npm/Busboy.hx
+++ js/npm/Busboy.hx
@@ -17,6 +17,24 @@ typedef Value=String;
 typedef FieldNameTruncated=Bool;
 typedef ValueTruncated=Bool;
 
+typedef BusboyConstructorLimits = {
+	@:optional var fieldNameSize :Float;
+	@:optional var fieldSize :Float;
+	@:optional var fields :Float;
+	@:optional var fileSize :Float;
+	@:optional var files :Float;
+	@:optional var parts :Float;
+	@:optional var headerPairs :Float;
+}
+
+typedef BusboyConstructorOpts = {
+	@:optional var headers :Dynamic;
+	@:optional var highWaterMark :Float;
+	@:optional var fileHwm :Float;
+	@:optional var defCharset :String;
+	@:optional var preservePath :Bool;
+	@:optional var limits :BusboyConstructorLimits;
+}
 
 /**
 	Enumeration of events emitted by the `Busboy` objects
@@ -25,6 +43,7 @@ typedef ValueTruncated=Bool;
 	var File : BusboyEvent<FieldName->Readable<Dynamic>->FileName->Encoding->MimeType->Void> = "file";
 	var Field : BusboyEvent<FieldName->Value->FieldNameTruncated->ValueTruncated->Void> = "field";
 	var Finish : BusboyEvent<Void->Void> = "finish";
+	var Limit : BusboyEvent<Void->Void> = "limit";
 	var PartsLimit : BusboyEvent<Void->Void> = "partsLimit";
 	var FilesLimit : BusboyEvent<Void->Void> = "filesLimit";
 	var FieldsLimit : BusboyEvent<Void->Void> = "fieldsLimit";
@@ -33,5 +52,5 @@ typedef ValueTruncated=Bool;
 @:jsRequire('busboy')
 extern class Busboy extends Writable<Busboy>
 {
-	public function new(opts :{headers:Dynamic}) :Void;
+	public function new(opts :BusboyConstructorOpts) :Void;
 }
