9d68e589991a3ef65a7912ef40f766c4c64dc522
Dion Amago <dion@transition9.com>
2016-05-08 20:29:16 -0700

    Update ssh2 to the better typed events

--- js/npm/Ssh.hx
+++ js/npm/Ssh.hx
@@ -1,9 +1,10 @@
 package js.npm;
 
 import js.Error;
-
+import js.node.events.EventEmitter.Event;
 import js.node.stream.Readable;
 import js.node.stream.Writable;
+import js.node.stream.Duplex;
 
 typedef ConnectOptions = {
 	var host :String;
@@ -40,13 +41,44 @@ typedef SshFile = {
 	var attrs :FileAttributes;
 }
 
+typedef SshSession=Dynamic;
+
+@:enum abstract SshConnectionEvent<T:haxe.Constraints.Function>(Event<T>) to Event<T> 
+{
+	var Authentication : SshConnectionEvent<Dynamic->Void> = 'authentication';
+	var Ready : SshConnectionEvent<Void->Void> = 'ready';
+	var Session : SshConnectionEvent<(Void->SshSession)->Void->Bool> = 'session';
+	var Continue : SshConnectionEvent<Void->Void> = 'continue';
+	var Error : SshConnectionEvent<Error->Void> = 'error';
+	var End : SshConnectionEvent<Void->Void> = 'end';
+	var Close : SshConnectionEvent<Bool->Void> = 'close';
+}
+
+@:enum abstract SshClientEvent<T:haxe.Constraints.Function>(Event<T>) to Event<T> 
+{
+	var Banner : SshClientEvent<String->String->Void> = 'banner';
+	var Ready : SshClientEvent<Void->Void> = 'ready';
+	var TcpConnection : SshClientEvent<Dynamic->Dynamic->Dynamic->Void> = 'tcp connection';
+	var X11 : SshClientEvent<Dynamic->Dynamic->Dynamic->Void> = 'x11';
+	var Continue : SshClientEvent<Void->Void> = 'continue';
+	var Error : SshClientEvent<Error->Void> = 'error';
+	var End : SshClientEvent<Void->Void> = 'end';
+	var Close : SshClientEvent<Void->Void> = 'close';
+}
+
+@:enum abstract SshExecEvent<T:haxe.Constraints.Function>(Event<T>) to Event<T> 
+{
+	var Ready : SshExecEvent<Void->Void> = 'ready';
+}
+
+@:enum abstract SshChannelEvent<T:haxe.Constraints.Function>(Event<T>) to Event<T> 
+{
+	var Close : SshChannelEvent<Int->String->Void> = 'close';
+}
+
 @:jsRequire("ssh2")
 extern class SshClient extends js.node.events.EventEmitter<Dynamic>
 {
-	public static inline var EVENT_READY = 'ready';
-	public static inline var EVENT_ERROR = 'error';
-	public static inline var EVENT_CONTINUE = 'continue';
-
 	public var stderr :js.node.events.EventEmitter<Dynamic>;
 	public var stdout :js.node.events.EventEmitter<Dynamic>;
 	public function new() :Void;
@@ -70,7 +102,7 @@ extern class SFTPStream extends js.node.events.EventEmitter<Dynamic>
 	public function mkdir(remotePath :String, ?attr :FileAttributes, cb :Null<Error>->Void) :Bool;
 }
 
-extern class ExecStream extends Readable<Dynamic>
+extern class ExecStream extends Duplex<Dynamic>
 {
 	public var stderr :IReadable;
 }
