333934e1c62dee27360fdf24a6a2ca7690d021e3
Dion Amago <dion@transition9.com>
2016-05-19 08:45:44 -0700

    Update request

--- js/npm/Request.hx
+++ js/npm/Request.hx
@@ -1,6 +1,7 @@
 package js.npm;
 
 import js.node.stream.Readable;
+import js.node.stream.Writable;
 
 typedef HttpResponse = {
 	var statusCode :Int;
@@ -10,6 +11,7 @@ typedef Body=String;
 @:jsRequire("request")
 extern class Request
 {
-	public static function post(postData :{url :String, formData :Dynamic}, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IReadable;
+	@:overload(function(url :String, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void):IWritable {})
+	public static function post(postData :{url :String, formData :Dynamic}, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IWritable;
 	public static function get(url :String, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IReadable;
 }
