32c1d904d8cce31d6b87a35c9eda6c2e53e01aa2
Dion Amago <dion@transition9.com>
2016-05-19 18:25:54 -0700

    update bunyan

--- js/npm/Bunyan.hx
+++ js/npm/Bunyan.hx
@@ -2,18 +2,18 @@ package js.npm;
 
 import haxe.extern.Rest;
 
-typedef Logger = {
+typedef BunyanLogger = {
 	function trace(args :Rest<Dynamic>) :Void;
 	function debug(args :Rest<Dynamic>) :Void;
 	function info(args :Rest<Dynamic>) :Void;
 	function warn(args :Rest<Dynamic>) :Void;
 	function error(args :Rest<Dynamic>) :Void;
 	function fatal(args :Rest<Dynamic>) :Void;
-	function child(fields :Dynamic) :Logger;
+	function child(fields :Dynamic) :BunyanLogger;
 }
 
 @:jsRequire("bunyan")
 extern class Bunyan extends js.node.events.EventEmitter<Dynamic>
 {
-	public static function createLogger(args :Dynamic) :Logger;
+	public static function createLogger(args :Dynamic) :BunyanLogger;
 }
