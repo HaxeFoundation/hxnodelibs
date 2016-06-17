a4f5862c41a2638fa6d6cdbbbc6e3682afe41866
Dion Amago <dion@transition9.com>
2016-06-17 16:58:48 -0700

    commander update

--- js/npm/Commander.hx
+++ js/npm/Commander.hx
@@ -9,8 +9,8 @@ extern class Commander
 	public var commands :Array<Command>;
 
 	public function version (s :String) :Commander;
-	public function command (s :String, ?description :String, ?options :Dynamic) :Command;
-	public function option(short :String, ?long :String, ?description :String) :Commander;
+	public function command (s :String, ?description :String, r:Rest<Dynamic>) :Command;
+	public function option(optionString :String, description :String, r:Rest<Dynamic>) :Commander;
 	public function parse (args :Array<String>) :Void;
 	public function help(?f :Void->Void) :Void;
 	public function outputHelp() :Void;
@@ -21,8 +21,7 @@ extern class Command
 	public var _name :String;
 	public function description(s :String) :Command;
 
-	@:overload(function (flags :String, description :String, coercion :Dynamic->Dynamic->Dynamic, defaultValue :Dynamic) :Command {})
-	@:overload(function (flags :String, description :String, coercion :Dynamic->Dynamic) :Command {})
+	@:overload(function (flags :String, description :String, coercion :Dynamic, ?defaultValue :Dynamic) :Command {})
 	@:overload(function (flags :String, description :String) :Command {})
 	public function option(flags :String) :Command;
 
