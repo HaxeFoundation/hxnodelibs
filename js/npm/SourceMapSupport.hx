de06e7ebba9e8103efd9341dbb7e68df0e16d8e0
Dion Amago <dion@transition9.com>
2016-06-30 16:45:06 -0700

    Fix for uncaught exceptions

--- js/npm/SourceMapSupport.hx
+++ js/npm/SourceMapSupport.hx
@@ -3,12 +3,13 @@ package js.npm;
 @:jsRequire("source-map-support")
 extern class SourceMapSupport
 {
-	public static function install() :Void;
+	public static function install(?opts :Dynamic) :Void;
 
 	private static function __init__() : Void untyped
 	{
 		var SourceMapSupport = Node.require('source-map-support');
-		SourceMapSupport.install();
+		//https://github.com/evanw/node-source-map-support/issues/40
+		SourceMapSupport.install({handleUncaughtExceptions: false});
 		//http://stackoverflow.com/questions/33778714/haxe-nodejs-debugging-source-mapping
 		haxe.CallStack.wrapCallSite = untyped __js__("require('source-map-support').wrapCallSite");
 	}
