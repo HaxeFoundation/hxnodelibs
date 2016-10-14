e49bd42a5a942008b2140556427d04faf8f9b672
Andy Li <andy@onthewings.net>
2016-10-14 11:33:38 +0800

    Added the missing base request method.

--- js/npm/request/Request.hx
+++ js/npm/request/Request.hx
@@ -1,5 +1,6 @@
 package js.npm.request;
 
+import js.node.Stream;
 import js.node.stream.Readable;
 import js.node.stream.Writable;
 
@@ -11,6 +12,7 @@ typedef Body=String;
 @:jsRequire("request")
 extern class Request
 {
+	@:selfCall public static function request(urlOrParams :Dynamic, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IStream;
 	public static function post(urlOrParams :Dynamic, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IWritable;
 	public static function get(urlOrParams :Dynamic, ?cb :Null<js.Error>->Null<HttpResponse>->Null<Body>->Void) :IReadable;
 }
