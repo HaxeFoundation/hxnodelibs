f5da54a73b91e7b2a8271d018911010e9bd21229
Dion Amago <dion@transition9.com>
2018-06-16 21:17:09 -0700

    Fix parse-duration name

--- js/npm/parseduration/ParseDuration.hx
+++ js/npm/parseduration/ParseDuration.hx
@@ -4,7 +4,7 @@ import haxe.extern.EitherType;
 
 // https://www.npmjs.com/package/parse-duration
 @:jsRequire("parse-duration")
-extern class Ms
+extern class ParseDuration
 {
 	@:selfCall
 	static function parse(t :String) :EitherType<Float,Int>;
